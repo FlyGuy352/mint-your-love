16915c6be31507bc8c380b4b0b0b02a12bb8b437
t14519 <tom.lin@salesforce.com>
2022-09-05 23:36:14 +0800

    commit

--- frontend/components/HeroImage.jsx
+++ frontend/components/HeroImage.jsx
@@ -7,10 +7,12 @@ export default function HeroImage() {
                 <h2>Immortalize Your Love on the Blockchain</h2>
             </div>
             <div className='row-start-3 text-center'>
-                <button className='px-6 py-4 relative rounded-2xl group overflow-hidden font-bold bg-white text-crimsonRed border border-crimsonRed'>
-                    <span className='absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-crimsonRed group-hover:h-full opacity-90'></span>
-                    <Link href='/myCollection'><span className='relative group-hover:text-white'>Start Minting</span></Link>
-                </button>
+                <Link href='/myCollection'>
+                    <button className='px-6 py-4 relative rounded-2xl group overflow-hidden font-bold bg-white text-crimsonRed border border-crimsonRed'>
+                        <span className='absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-crimsonRed group-hover:h-full opacity-90'></span>
+                        <span className='relative group-hover:text-white'>Start Minting</span>
+                    </button>
+                </Link>
             </div>
         </div>
     );
